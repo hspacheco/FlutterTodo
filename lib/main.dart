2a49e5fab58b546cf5321fc15ef170d4843890f4
Henrique Pacheco <henrique.pacheco@cubos.io>
2019-09-19 01:01:40 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+enum Action { edit, delete }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'TodoApp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class TodoItem {
+  String title;
+  bool status;
+
+  TodoItem({this.title, this.status = false});
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  List _items = List<TodoItem>();
+  List _itemsDone = List<TodoItem>();
+  final textController = TextEditingController();
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
+
+  void _addTodoItem(TodoItem item) {
+    setState(() {
+      _items.add(item);
+    });
+  }
+
+  void _addDoneItem(int index, bool status) {
+    setState(() {
+      var item = _items[index];
+      item.status = true;
+      _itemsDone.add(item);
+      _items.removeRange(index, index + 1);
+    });
+  }
+
+  Widget _renderItemsList(List list) {
+    return (Center(
+      child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Checkbox(
+                            value: list[index].status,
+                            onChanged: (bool val) {
+                              if (val) {
+                                _addDoneItem(index, val);
+                              }
+                            }),
+                        Text(
+                          '${list[index].title}',
+                          style: list[index].status
+                              ? TextStyle(
+                                  decoration: TextDecoration.lineThrough,
+                                  color: Colors.grey,
+                                )
+                              : null,
+                        ),
+                      ],
+                    ),
+                    margin: EdgeInsets.only(right: 6),
+                  ),
+                  PopupMenuButton<Action>(
+                    onSelected: (Action result) {},
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<Action>>[
+                      const PopupMenuItem<Action>(
+                        value: Action.edit,
+                        child: Text('Editar'),
+                      ),
+                      const PopupMenuItem<Action>(
+                        value: Action.delete,
+                        child: Text('Apagar'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider()),
+    ));
+  }
+
+  Future<void> _openDialog(BuildContext context) async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text('Nova Tarefa'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: TextField(
+                  controller: textController,
+                  autofocus: true,
+                  onChanged: (String val) {},
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Adicionar Tarefa',
+                  ),
+                ),
+              ),
+              SimpleDialogOption(
+                child: FlatButton(
+                  child: Text('Adicionar'),
+                  onPressed: () {
+                    var item = TodoItem(title: textController.text);
+                    _addTodoItem(item);
+                    textController.clear();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(text: "Tarefas"),
+            Tab(text: "Arquivadas"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: _renderItemsList(_items),
+          ),
+          Center(
+            child: _renderItemsList(_itemsDone),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _openDialog(context);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
